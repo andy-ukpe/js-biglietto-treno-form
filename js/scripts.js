7da1f019d9a7cefdfe515da8c784376c0655e9e6
andy <andy.ukpe@outlook.com>
2020-05-21 20:16:40 +0200

    completata la parte di calcolo del biglietto

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,50 @@
 var generateButton = document.getElementById('Generate');
 var nullButton = document.getElementById('annulla');
 var formName = document.getElementById('form-Name');
+var formKm = document.getElementById('form-Km');
+var etaViaggiatore = document.getElementById('fasciaEta');
+
 
 // elementi Biglietto
 var biglName = document.getElementById('bigl-Name');
+var biglCosto = document.getElementById('bigl-Cost');
+
+// altro
+var formKmValue;
+var costoValue
 
 // evento quando si clicca su genera
 generateButton.addEventListener('click',
   function(){
+
+    // prezzo unitario
+    formKmValue = formKm.value;
+    etaViaggiatore = etaViaggiatore.value;
+    var prezzoUnitario = formKmValue * 0.21;
+
+    if( etaViaggiatore == 'Minorenne' ) {
+
+      // prezzo junior
+      var prezzoJunior = prezzoUnitario - (  prezzoUnitario * 20 / 100 );
+      console.log(prezzoJunior);
+      biglCosto.innerHTML = prezzoJunior.toFixed(2) + '€';
+
+
+   } else if( etaViaggiatore == 'Over 65' ) {
+
+      //prezzo major
+      var prezzoMajor = prezzoUnitario - ( ( prezzoUnitario * 40) / 100 );
+
+      biglCosto.innerHTML = prezzoMajor.toFixed(2) + '€';
+
+   } else {
+
+      //Costo
+      costoValue = prezzoUnitario.toFixed(2);
+      biglCosto.innerHTML = prezzoUnitario + '€';
+
+   }
+
     // nome sul Biglietto
     biglName.innerHTML = formName.value;
 
@@ -21,5 +58,11 @@ nullButton.addEventListener('click',
     // annulla nome
     formName.value = '';
     biglName.innerHTML = '';
+    // annulla km
+    formKm.value = '';
+    // fasciaEta
+    fasciaEta.value = 'Maggiorenne';
+    // prezzo
+    biglCosto.innerHTML = '';
   }
 );
