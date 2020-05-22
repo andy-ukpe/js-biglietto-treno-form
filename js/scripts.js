0e1a1317c32a48a543bac24b403f2f474bcb2f23
andy <andy.ukpe@outlook.com>
2020-05-22 22:05:47 +0200

    aggiunta funzione display block none

--- js/scripts.js
+++ js/scripts.js
@@ -7,59 +7,79 @@ var formName = document.getElementById('form-Name');
 var formKm = document.getElementById('form-Km');
 var etaViaggiatore = document.getElementById('fasciaEta');
 
+// variabile display
+var display = document.getElementById('stampa-biglietto');
+
 // elementi Biglietto
 var biglName = document.getElementById('bigl-Name');
 biglName.trim;
 var biglCosto = document.getElementById('bigl-Cost');
-var offerta = document.getElementById('bigl-off');
+var biglOfferta = document.getElementById('bigl-off');
+var biglCarrozza = document.getElementById('bigl-car');
+var biglCodice = document.getElementById('bigl-Code');
 
 // altro
 var formKmValue;
 var costoValue;
-
+var costo;
 
 // evento quando si clicca su genera
 generateButton.addEventListener('click',
   function(){
 
-    // prezzo unitario
-    formKmValue = parseInt(formKm.value);
-    formEtaValue = etaViaggiatore.value;
-    var prezzoUnitario = formKmValue * 0.21;
+      display.style.display="block";
 
-    if( formEtaValue == 'Minorenne' ) {
+      // prezzo unitario
+      formKmValue = parseInt(formKm.value);
+      formEtaValue = etaViaggiatore.value;
+      var prezzoUnitario = formKmValue * 0.21;
 
-      // prezzo junior
-      var prezzoJunior = prezzoUnitario - (  prezzoUnitario * 20 / 100 );
-      console.log(prezzoJunior);
-      biglCosto.innerHTML = prezzoJunior.toFixed(2) + '€';
-      offerta.innerHTML = 'offerta Junior';
+      if( formEtaValue == 'Minorenne' ) {
 
-   } else if( formEtaValue == 'Over 65' ) {
+        // prezzo junior
+        costo = prezzoUnitario - (  prezzoUnitario * 20 / 100 );
+        // offerta
+        offerta = 'offerta Junior';
 
-      //prezzo major
-      var prezzoMajor = prezzoUnitario - ( ( prezzoUnitario * 40) / 100 );
+     } else if( formEtaValue == 'Over 65' ) {
 
-      biglCosto.innerHTML = prezzoMajor.toFixed(2) + '€';
-      offerta.innerHTML = 'offerta Silver';
-   } else {
+        //prezzo major
+        costo = prezzoUnitario - ( ( prezzoUnitario * 40) / 100 );
+        // offerta
+        offerta = 'offerta Silver';
+     } else {
 
-      //Costo
-      costoValue = prezzoUnitario.toFixed(2);
-      biglCosto.innerHTML = costoValue + '€';
-      offerta.innerHTML = '/';
-   }
+        //Costo
+        costo = prezzoUnitario;
+        // offerta
+        offerta = '/';
+     }
 
-    // nome sul Biglietto
-    biglName.innerHTML = formName.value;
+     // STAMPA BIGLIETTO
+     // nome sul Biglietto
+     biglName.innerHTML = formName.value;
+     // offerta
+     biglOfferta.innerHTML = offerta ;
+     // Carrozza
+     biglCarrozza.innerHTML = Math.floor(Math.random() * 20 ) + 1 ;
+     // Codice
+     biglCodice.innerHTML = Math.floor(Math.random() * 100000 ) + 1 ;
+     // costo
+     biglCosto.innerHTML = costo.toFixed(2) + '€';
+
+     // display-block
+     display.className = display.classList + " display-block";
 
     }
+
 );
 
 // evento quando si clicca ANNULLA
 nullButton.addEventListener('click',
   function(){
+    // RESET BIGLIETTO
     // annulla nome
+    display.style.display="none";
     formName.value = '';
     biglName.innerHTML = '';
     // annulla km
@@ -68,7 +88,13 @@ nullButton.addEventListener('click',
     fasciaEta.value = 'Maggiorenne';
     // Offerta
     offerta.innerHTML = '';
+    // Carrozza
+    biglCarrozza.innerHTML = '' ;
+    // Codice
+    biglCodice.innerHTML = '' ;
     // prezzo
     biglCosto.innerHTML = '';
+    // display
+    display.className = display.classList + " display-none";
   }
 );
